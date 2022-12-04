adc2148efdc58efea3837e65eec0f095f73c8f4c
Bryan Moffatt <bryan@bryanmoffatt.com>
2022-12-03 22:39:27 -0800

    add Be(T, bool) T

--- must.go
+++ must.go
@@ -1,9 +1,18 @@
 package must
 
+import "errors"
+
 func Do(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func Be[T any](t T, ok bool) T {
+	if !ok {
+		panic(errors.New("not ok!"))
+	}
+	return t
 }
 
 func Return[T any](t T, err error) T {
